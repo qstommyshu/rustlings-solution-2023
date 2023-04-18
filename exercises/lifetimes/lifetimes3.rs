237e2653191fa662ac5a90a7047a982046210f25
qstommyshu <qstommyshu@gmail.com>
2023-04-18 10:47:20 -0400

    feat: slove lifetimes3

--- exercises/lifetimes/lifetimes3.rs
+++ exercises/lifetimes/lifetimes3.rs
@@ -4,17 +4,22 @@
 //
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-struct Book {
-    author: &str,
-    title: &str,
+// Explanation: this question is just the syntax of lifetime for structs that holds
+//references. I feel like the question just generally become much easier after errors6
+//for some reason, most of them are just straight forward from book, doesn't quite
+//require me to actually understand the concept. Or maybe I became better?
+struct Book<'a> {
+    author: &'a str,
+    title: &'a str,
 }
 
 fn main() {
     let name = String::from("Jill Smith");
     let title = String::from("Fish Flying");
-    let book = Book { author: &name, title: &title };
+    let book = Book {
+        author: &name,
+        title: &title,
+    };
 
     println!("{} by {}", book.title, book.author);
 }
